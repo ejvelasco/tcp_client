5fa8dbc584af330adcc7500a4daef6acf33643ec
velascoDev <dracontium2@gmail.com>
2017-05-24 10:43:29 -0500

    Removed practice files

--- generateRandom.js
+++
@@ -1,8 +0,0 @@
-const RandomStream = require("./randomStream");
-const randomStream = new RandomStream();
-randomStream.on("readable", () => {
-	let chunk;
-	while((chunk = randomStream.read()) !== null){
-		console.log("Chunk received: " + chunk.toString());
-	}
-});
